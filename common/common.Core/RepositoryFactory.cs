b16846820ecf35c36865744a7628abdd7a6aa9cd
bigbigtomcat <46481721+bigbigtomcat@users.noreply.github.com>
2022-05-10 11:40:06 +0800

    代码提交
    框架

---
+++ common/common.Core/RepositoryFactory.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace common.Core
+{
+  public  class RepositoryFactory
+    {
+    }
+}
---
+++ service/common.sync.IService/IBaseService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace common.sync.IService
+{
+   public class IBaseService
+    {
+
+    }
+}
